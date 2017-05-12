b1aa4112a246dce569d03347ead6b1d0ad61c486
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-12 14:51:29 -0700

    Add comments to dom-util

--- dom-util.js
+++ dom-util.js
@@ -2,6 +2,7 @@
 const visitAllNodes = function (node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
   // Your code here
+  // TODO: fix!!! No test is passing so far
   let children = node.childNodes;
   if (children.hasChildNodes()) {
     for (let i = 0; i < children.length; i++) {
@@ -15,6 +16,7 @@ const visitAllNodes = function (node, callback) {
 const flattenTreeToArray = function (node) {
   // Hint: Use visitAllNodes()
   // Your code here
+  // TODO: fix!!! No test is passing so far
   const flatArr = [];
   visitAllNodes(node, (child) => flatArr.push(child));
   return flatArr;
