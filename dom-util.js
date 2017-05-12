10c29299b9f75b05956496b03d40d369a766b59f
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-12 14:41:59 -0700

    Add visitAllNodes()

--- dom-util.js
+++ dom-util.js
@@ -1,15 +1,23 @@
 
-const visitAllNodes = function(node, callback) {
+const visitAllNodes = function (node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
   // Your code here
+  let children = node.childNodes;
+  if (node.hasChildNodes()) {
+    for (let i = 0; i < children.length; i++) {
+      visitAllNodes(children[i], callback);
+    }
+  }
+
+  callback(children);
 };
 
-const flattenTreeToArray = function(node) {
+const flattenTreeToArray = function (node) {
   // Hint: Use visitAllNodes()
   // Your code here
 };
 
 module.exports = {
   visitAllNodes: visitAllNodes,
-  flattenTreeToArray: flattenTreeToArray
+  flattenTreeToArray: flattenTreeToArray,
 };
--- dominator.js
+++ dominator.js
@@ -1,20 +1,20 @@
 const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
-const getElementById = function(root, id) {
+const getElementById = function (root, id) {
   // Your code here
 };
 
-const getElementsByClassName = function(root, className) {
+const getElementsByClassName = function (root, className) {
   // Your code here
 };
 
-const getElementsByTagName = function(root, tagName) {
+const getElementsByTagName = function (root, tagName) {
   // Your code here
 };
 
 module.exports = {
   getElementById: getElementById,
   getElementsByClassName: getElementsByClassName,
-  getElementsByTagName: getElementsByTagName
+  getElementsByTagName: getElementsByTagName,
 };
