32388c3a4225c9130f246f64afde7b780b5f226e
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-12 15:46:44 -0700

    Add getElementByClassName()

--- dominator.js
+++ dominator.js
@@ -4,15 +4,24 @@ const _ = require('underscore');
 const getElementById = function (root, id) {
   // Your code here
   const flatArr = flattenTreeToArray(root);
-  return flatArr.filter((elem) => {
-    if (flatArr[elem] === id) {
-      return id;
+  return flatArr.map((elem) => {
+    if (flatArr[elem].id === id) {
+      return flatArr[elem];
     }
   });
 };
 
 const getElementsByClassName = function (root, className) {
   // Your code here
+  const flatArr = flattenTreeToArray(root);
+  let classNameArr = [];
+  flatArr.map((elem) => {
+    if (flatArr[elem].className && (flatArr[elem].className).indexOf(className) !== -1) {
+      classNameArr.push(flatArr[elem]);
+    }
+  });
+
+  return classNameArr;
 };
 
 const getElementsByTagName = function (root, tagName) {
